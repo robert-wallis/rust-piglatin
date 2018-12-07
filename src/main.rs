cef39fd20310bd84bd6d27dca7b803287628a44f
Robert Wallis <smilingrob@gmail.com>
2018-12-07 08:14:35 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+// Copyright (C) 2018 Robert A. Wallis
+// Convert strings to pig latin.
+//      The first consonant of each word is moved to the end of the word
+//      and “ay” is added, so “first” becomes “irst-fay.”
+//      Words that start with a vowel have “hay” added to the end instead
+//      (“apple” becomes “apple-hay”).
+//      Keep in mind the details about UTF-8 encoding!
+// - https://doc.rust-lang.org/book/ch08-03-hash-maps.html
+
+fn main() {
+    println!("Pig Latin");
+}
